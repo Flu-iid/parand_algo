0b81f64054fc8576ddbc0bb987e2b83d97ac472f
flu <57827666+Flu-iid@users.noreply.github.com>
2024-03-13 03:18:30 +0330

    added 08 answer

---
+++ answers/08A-N.py
@@ -0,0 +1,23 @@
+def string_sum(s: str) -> str:
+    if len(s) == 1:
+        return s
+    result = 0
+    for c in s:
+        result += int(c)
+    return string_sum(str(result))
+
+
+n = input()
+print(string_sum(n))
+
+
+# 2
+
+def crazy_sum(n: int) -> int:
+    if n//10 == 0:  # n will be between 0 - 9
+        return n
+    return crazy_sum(sum([int(c) for c in str(n)]))
+
+
+n = int(input())
+print(crazy_sum(n))
