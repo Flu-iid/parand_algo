9474669b770832c178d150f3790c6b8f878a0564
flu <57827666+Flu-iid@users.noreply.github.com>
2024-03-26 13:18:44 +0330

    some adjustment to code order

--- answers/12A-N.cpp
+++ answers/12A-N.cpp
@@ -1,4 +1,34 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+    string a, b;
+    cin >> a >> b;
+
+    int n = (a[2] - '0') * 100 + (a[1] - '0') * 10 + (a[0] - '0');
+    int m = (b[2] - '0') * 100 + (b[1] - '0') * 10 + (b[0] - '0');
+
+    if (n < m)
+    {
+        cout << a << " < " << b;
+    }
+    else if (m < n)
+    {
+        cout << b << " < " << a;
+    }
+    else
+    {
+        cout << a << " = " << b;
+    }
+}
+
+// fatemeh_darvishi @M_darvishi_f
+
+//
+
+#include <iostream>
 using namespace std;
 
 int main()
@@ -27,33 +57,3 @@ int main()
 }
 
 // arsham_mahdiun @Arshamm1997
-
-//
-
-#include <iostream>
-#include <string>
-
-using namespace std;
-int main()
-{
-    string a, b;
-    cin >> a >> b;
-
-    int n = (a[2] - '0') * 100 + (a[1] - '0') * 10 + (a[0] - '0');
-    int m = (b[2] - '0') * 100 + (b[1] - '0') * 10 + (b[0] - '0');
-
-    if (n < m)
-    {
-        cout << a << " < " << b;
-    }
-    else if (m < n)
-    {
-        cout << b << " < " << a;
-    }
-    else
-    {
-        cout << a << " = " << b;
-    }
-}
-
-// fatemeh_darvishi @M_darvishi_f
